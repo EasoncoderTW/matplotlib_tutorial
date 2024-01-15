09700214fc23d7ba9c438d45918c92874a6c5c8e
EasoncoderTW <easonyehmobile1@gmail.com>
2024-01-15 15:24:42 +0800

    add axes for program14

--- program14.py
+++ program14.py
@@ -5,9 +5,10 @@ import numpy as np
 x = np.linspace(0, 2 * np.pi, 400)
 y = np.sin(x ** 2)
 
-fig, (ax1, ax2) = plt.subplots(2,sharex=True,sharey=True) # 共用X軸標籤
+fig, (ax1, ax2, ax3) = plt.subplots(3,sharex=True,sharey=True) # 共用X軸標籤
 fig.suptitle('Vertically stacked subplots')
 ax1.plot(x, y)
 ax2.plot(x, -y/2)
+ax3.plot(x, y*2)
 
 plt.show()
